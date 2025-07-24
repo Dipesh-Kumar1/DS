0f040cc12921a9ffc72c71adcc9e1fa7cff3bf5e
Dipesh Kumar <164227523+Dipesh-Kumar1@users.noreply.github.com>
2025-07-25 02:37:25 +0530

    Add files via upload

--- swap.c
+++ swap.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
+int swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+    
+}
 int main(){
     int a,b,temp;
-    printf("enter the value of a ");
-    scanf("%d",&a);
-    printf("enter the value of ");
-     scanf("%d",&b);
-
-    temp=a;
-    a=b;
-    b=temp;
-    printf("the number a after swap a is %d and b is %d",a,b);
+    printf("enter the value of a,b");
+    scanf("%d%d",&a,&b);
+    swap(&a, &b);
+    printf("the number after swap is %d%d",a,b);
     return 0;
-
 
 }
