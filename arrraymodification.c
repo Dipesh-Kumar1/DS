09ea6131760068c1889baadfc7a6dced09d16bb0
Dipesh Kumar <164227523+Dipesh-Kumar1@users.noreply.github.com>
2025-08-03 13:55:32 +0530

    Add files via upload

---
+++ arrraymodification.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int arr[]={10,20,30,40,50};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        arr[i]=arr[i]*2;
+    }
+        for(int i=0;i<n;i++){
+            printf("%d\n",arr[i]);
+        
+    }
+    return 0;
+}
